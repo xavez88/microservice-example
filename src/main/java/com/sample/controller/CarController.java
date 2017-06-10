17c4f6c2f8f07aa1e8167ae53b7a923f03f1d626
iron-mac <antonioluischavez@gmail.com>
2017-06-10 12:54:03 +0100

    Add controller to handle car requests

---
+++ src/main/java/com/sample/controller/CarController.java
@@ -0,0 +1,16 @@
+package com.sample.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by antoniochavez on 10/06/2017.
+ *
+ *  Controller class to handle all car related requests.
+ *
+ */
+
+@RequestMapping
+public class CarController {
+
+
+}
