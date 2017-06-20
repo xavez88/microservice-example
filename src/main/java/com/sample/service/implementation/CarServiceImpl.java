8c8646887a63411640d73ca8e2d57afb27d9b964
iron-mac <antonioluischavez@gmail.com>
2017-06-20 17:47:43 +0100

    Add service methods and service implementations

---
+++ src/main/java/com/sample/service/implementation/CarServiceImpl.java
@@ -0,0 +1,10 @@
+package com.sample.service.implementation;
+
+/**
+ * Created by antoniochavez on 20/06/2017.
+ *
+ * Real implementation for {@link com.sample.service.CarService}
+ *
+ */
+public class CarServiceImpl {
+}
