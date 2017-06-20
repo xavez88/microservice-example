559535050261065c8e9998fc11c5cc30dc657e92
iron-mac <antonioluischavez@gmail.com>
2017-06-20 08:26:29 +0100

    Minor amendment to API definition and add annotations to CarController

--- src/main/java/com/sample/controller/CarController.java
+++ src/main/java/com/sample/controller/CarController.java
@@ -1,7 +1,9 @@
 package com.sample.controller;
 
+import com.sample.dto.CarDto;
 import com.sample.utils.UrlConstants;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by antoniochavez on 10/06/2017.
@@ -12,6 +14,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(UrlConstants.CARS_ROOT_MAPPING)
 public class CarController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CarDto create(){
+        return null;
+    }
+
+    @GetMapping(UrlConstants.CAR_ID_MAPPING)
+    @ResponseStatus(HttpStatus.OK)
+    public CarDto read(){
+        return null;
+    }
+
+    @PutMapping(UrlConstants.CAR_ID_MAPPING)
+    @ResponseStatus(HttpStatus.OK)
+    public CarDto update(){
+        return null;
+    }
+
+    @PatchMapping(UrlConstants.CAR_ID_MAPPING)
+    @ResponseStatus(HttpStatus.OK)
+    public CarDto updatePartially(){
+        return null;
+    }
+
+    @DeleteMapping(UrlConstants.CAR_ID_MAPPING)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(){
+
+    }
 
 
 }
