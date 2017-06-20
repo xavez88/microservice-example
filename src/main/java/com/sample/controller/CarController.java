31d4b1207cdd758c5e540c7f99e98c0320b3633b
iron-mac <antonioluischavez@gmail.com>
2017-06-20 08:31:53 +0100

    Add controller PathVariables and Service interface

--- src/main/java/com/sample/controller/CarController.java
+++ src/main/java/com/sample/controller/CarController.java
@@ -5,6 +5,8 @@ import com.sample.utils.UrlConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 /**
  * Created by antoniochavez on 10/06/2017.
  *
@@ -17,31 +19,33 @@ public class CarController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CarDto create(){
+    public CarDto create(@RequestBody CarDto car){
         return null;
     }
 
     @GetMapping(UrlConstants.CAR_ID_MAPPING)
     @ResponseStatus(HttpStatus.OK)
-    public CarDto read(){
+    public CarDto read(@PathVariable("carId") String carId){
         return null;
     }
 
     @PutMapping(UrlConstants.CAR_ID_MAPPING)
     @ResponseStatus(HttpStatus.OK)
-    public CarDto update(){
+    public CarDto update(@PathVariable("carId") String carId,
+                         @RequestBody CarDto car){
         return null;
     }
 
     @PatchMapping(UrlConstants.CAR_ID_MAPPING)
     @ResponseStatus(HttpStatus.OK)
-    public CarDto updatePartially(){
+    public CarDto updatePartially(@PathVariable("carId") String carId,
+                                  Map<String,Object> args){
         return null;
     }
 
     @DeleteMapping(UrlConstants.CAR_ID_MAPPING)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(){
+    public void delete(@PathVariable("carId") String carId){
 
     }
 
