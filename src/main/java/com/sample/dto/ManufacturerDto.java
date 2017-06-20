d4da801eb6128863dea325739046ed76bd331e48
iron-mac <antonioluischavez@gmail.com>
2017-06-20 08:17:56 +0100

    Add properties to the DTOs

--- src/main/java/com/sample/dto/ManufacturerDto.java
+++ src/main/java/com/sample/dto/ManufacturerDto.java
@@ -7,4 +7,44 @@ package com.sample.dto;
  *
  */
 public class ManufacturerDto {
+
+    private String id;
+
+    private String name;
+
+    private ContactDetailsDto contactDetails;
+
+    private AddressDto address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ContactDetailsDto getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(ContactDetailsDto contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public AddressDto getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressDto address) {
+        this.address = address;
+    }
 }
