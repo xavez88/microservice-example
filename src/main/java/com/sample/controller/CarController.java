2778c37b329ab57b7eb2bf109d0606e1a4b95231
iron-mac <antonioluischavez@gmail.com>
2017-06-25 23:28:40 +0100

    Add controller stub implementation

--- src/main/java/com/sample/controller/CarController.java
+++ src/main/java/com/sample/controller/CarController.java
@@ -1,7 +1,9 @@
 package com.sample.controller;
 
 import com.sample.dto.CarDto;
+import com.sample.service.CarService;
 import com.sample.utils.UrlConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,44 +11,51 @@ import java.util.Map;
 
 /**
  * Created by antoniochavez on 10/06/2017.
- *
- *  Controller class to handle all car related requests.
- *
+ * <p>
+ * Controller class to handle all car related requests.
  */
 
 @RequestMapping(UrlConstants.CARS_ROOT_MAPPING)
 public class CarController {
 
+    @Autowired
+    private CarService carService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CarDto create(@RequestBody CarDto car){
-        return null;
+    public CarDto create(@RequestBody CarDto car) {
+
+        return carService.create(car);
     }
 
     @GetMapping(UrlConstants.CAR_ID_MAPPING)
     @ResponseStatus(HttpStatus.OK)
-    public CarDto read(@PathVariable("carId") String carId){
-        return null;
+    public CarDto read(@PathVariable("carId") String carId) {
+
+        return carService.find(carId);
     }
 
     @PutMapping(UrlConstants.CAR_ID_MAPPING)
     @ResponseStatus(HttpStatus.OK)
     public CarDto update(@PathVariable("carId") String carId,
-                         @RequestBody CarDto car){
-        return null;
+                         @RequestBody CarDto car) {
+
+        return carService.update(car);
     }
 
     @PatchMapping(UrlConstants.CAR_ID_MAPPING)
     @ResponseStatus(HttpStatus.OK)
     public CarDto updatePartially(@PathVariable("carId") String carId,
-                                  Map<String,Object> args){
-        return null;
+                                  Map<String, Object> args) {
+
+        return carService.partialUpdate(carId,args);
     }
 
     @DeleteMapping(UrlConstants.CAR_ID_MAPPING)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable("carId") String carId){
+    public void delete(@PathVariable("carId") String carId) {
 
+        carService.delete(carId);
     }
 
 
